e5a10375b8a82c3a82cdfe04bf3a21c9a17f6fc2
21lsparks <21lsparks@athenian.org>
2020-09-24 22:41:26 +0000

    it is done and we are geniuses

--- my_code.py
+++ my_code.py
@@ -1,19 +1,18 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  megan
 
 def find_gcf(x,y):   # Do not change function name!
     # User code goes here
-
-
-    return gcf
-
+    for count in range(x, 0, -1):
+        if x%count==0 and y%count==0:
+            return count
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(find_gcf(6,9))
+    #print(find_gcf(6,9))
 
     # After you are satisfied with your results, use input() to prompt the user for two values:
-    #x = int(input("Enter a number: "))
-    #y = int(input("Enter another number: "))
-
+    x = int(input("Enter a number: "))
+    y = int(input("Enter another number: "))
+    print(find_gcf(x,y))
